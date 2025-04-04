9f5e36455ad74649fff0858de4dc5e5d2cf8bc90
usnuz <vipfthef@gmail.com>
2025-04-04 18:24:36 +0500

    a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django_bot",
-    version="0.1.22",
+    version="0.1.23",
     author="Shokhzodbek",
     author_email="vipfthef@gmail.com",
     packages=find_packages(),
